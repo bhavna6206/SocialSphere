140e9ff83832ff002d0d3e04e13d6bb0dfbf5eab
Bhavna <bhavna6206@gmail.com>
2026-07-31 17:58:29 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   })
 );
